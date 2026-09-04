e9fad77123c948e5566dd849612bb352bb305d81
Gkan23 <gerald.canales1@tecnacional.edu.ni>
2026-09-04 14:12:09 -0600

    feat(portal): add route completion workflow (revision)

--- tests/Feature/Http/Controllers/PortalRouteCompletionControllerTest.php
+++ tests/Feature/Http/Controllers/PortalRouteCompletionControllerTest.php
@@ -778,7 +778,7 @@ class PortalRouteCompletionControllerTest extends TestCase
 
         $this->assertSame(
             $before,
-            $this->snapshot($scenario)4
+            $this->snapshot($scenario)
         );
     }
 }
